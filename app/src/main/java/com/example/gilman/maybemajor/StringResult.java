7f0cded3903718a4e9a9c5caea6fc43501f61e92
Mandeep <gillmandeep74@gmail.com>
2018-11-17 23:32:49 +0530

    added button to copy text to clipboard for not custom text and icon change

--- app/src/main/java/com/example/gilman/maybemajor/StringResult.java
+++ app/src/main/java/com/example/gilman/maybemajor/StringResult.java
@@ -1,12 +1,20 @@
 package com.example.gilman.maybemajor;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class StringResult extends AppCompatActivity {
+    String text;
     TextView textView;
+    Button copyButton;
     Intent intent;
     QRDatabase qrDatabase=QRDatabase.getqrDatabase(this);
     QRDao qrDao=qrDatabase.getQrDao();
@@ -15,10 +23,19 @@ public class StringResult extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_string_result);
         textView=findViewById(R.id.resultText);
+        copyButton=findViewById(R.id.copyButton);
         intent=getIntent();
-        String text=intent.getStringExtra("data");
+        text=intent.getStringExtra("data");
         textView.setText(text);
 //        SavedData dbData1=new SavedData(qrDao.getmaxId()+1, text);
 //        qrDao.addEntity(dbData1);
+    }
+
+    public void copyContent(View view){
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label",text);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(this, "Copied to Clipboard", Toast.LENGTH_LONG).show();
     }
 }
